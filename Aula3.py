2419fe84ffc59f05453a95e89f8d77a8ae9703a0
unknown <https://github.com/BarbaraAFernandes>
2020-01-25 08:36:56 -0300

    while

---
+++ Aula3.py
@@ -0,0 +1,37 @@
+#Estrutura de Repetições.
+#Laços While.
+
+'''
+numero = 0
+while numero <= 10:
+    print (numero)
+    numero = numero + 1
+
+numero = 0
+while numero <= 10:
+    print (numero)
+    numero = numero + 1
+else:
+    print('Acabou o laço While')
+
+'''
+##Looping infinito
+'''
+numero = 0
+while numero <=10:
+    print(numero)
+    break
+
+'''
+#break é para parar um looping infinito
+##Tabuada
+
+tabuada = int(input("Insira o numero da tabuada"))
+multiplicador = 2
+while (multiplicador <= 10):
+    print(tabuada * multiplicador )
+multiplicador = multiplicador + 1
+else:
+    print("Fim da Tabuada de ",tabuada)
+
+
